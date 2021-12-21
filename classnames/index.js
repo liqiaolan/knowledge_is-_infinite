9921d0c90068fa6255804990828371883b67af4c
hahaguo <hahaguo@myweimai.com>
2021-12-21 21:22:32 +0800

    feat: classnames包完成

---
+++ classnames/index.js
@@ -0,0 +1,66 @@
+/**
+ * classNames  传参类型为 number string object arr
+ * 循环本来使用了  let i = len; i--; 出来的顺序单元测试不通过，需要翻转才可以所以修改为正序列
+ */
+
+const type = (arg, str) =>
+  Object.prototype.toString.call(arg) === `[object ${str}]`;
+
+const classNames = (...arg) => {
+  let classArr = [],
+    len = arg.length;
+
+  for (let i = 0; i < len; i++) {
+    let element = arg[i];
+    /**
+     * element 的判断是将null undefined去掉，==的判断转换
+     */
+    if (
+      (typeof element === "string" || typeof element === "number") &&
+      element
+    ) {
+      classArr.push(element);
+    } else {
+      switch (true) {
+        case type(element, "Object"):
+          /**
+           * 如果有addPrefixCls函数 实现在类名前拼接
+           */
+          if (
+            element.hasOwnProperty("addPrefixCls") &&
+            type(element["addPrefixCls"], "Function")
+          ) {
+            for (let j in element) {
+              if (element[j] && j !== "addPrefixCls") {
+                classArr.push(element.addPrefixCls() + j);
+              }
+            }
+          } else {
+            for (let j in element) {
+              if (element[j]) {
+                classArr.push(j);
+              }
+            }
+          }
+          break;
+        case type(element, "Array"):
+          element.forEach((el) => {
+            /**
+             * 判断是不是为空字符串的原因
+             * classNames(['a', 0, null, undefined, false, true, 'b']
+             * 直接split会出现[''],想用正则没写出来【.split(/^\s(' ')\s$/g))
+             */
+            classArr = classArr.concat(
+              classNames(el) !== "" ? classNames(el).split(" ") : []
+            );
+          });
+          break;
+      }
+    }
+  }
+  console.log(classArr, "classArrclassArr");
+  console.log(classArr.join(" "), "classStr");
+  return classArr.join(" ");
+};
+
+module.exports = classNames;
---
+++ classnames/index.test.js
@@ -0,0 +1,74 @@
+const classNames = require("./index");
+
+test("Realize classnames exactly", () => {
+  expect(
+    classNames({
+      a: true,
+      b: false,
+      c: 0,
+      d: null,
+      e: undefined,
+      f: 1,
+    })
+  ).toBe("a f");
+  expect(classNames("a", 0, null, undefined, true, 1, "b")).toBe("a 1 b");
+  expect(classNames({ a: true }, "b", 0)).toBe("a b");
+  expect(classNames("", "b", {}, "")).toBe("b");
+  expect(classNames({})).toBe("");
+  expect(classNames(["a", "b"])).toBe("a b");
+  expect(classNames(["a", "b"], "c")).toBe("a b c");
+  expect(classNames("c", ["a", "b"])).toBe("c a b");
+  expect(classNames(["a", "b"], ["c", "d"])).toBe("a b c d");
+  expect(classNames(["a", 0, null, undefined, false, true, "b"])).toBe("a b");
+  expect(classNames(["a", ["b", "c"]])).toBe("a b c");
+  expect(classNames(["a", { b: true, c: false }])).toBe("a b");
+  expect(classNames(["a", ["b", ["c", { d: true }]]])).toBe("a b c d");
+  expect(classNames("a", [])).toBe("a");
+  expect(classNames("a", [[]])).toBe("a");
+  expect(
+    classNames({
+      null: null,
+      emptyString: "",
+      noNumber: NaN,
+      zero: 0,
+      negativeZero: -0,
+      false: false,
+      undefined: undefined,
+      nonEmptyString: "foobar",
+      whitespace: " ",
+      function: Object.prototype.toString,
+      emptyObject: {},
+      nonEmptyObject: { a: 1, b: 2 },
+      emptyList: [],
+      nonEmptyList: [1, 2, 3],
+      greaterZero: 1,
+    })
+  ).toBe(
+    "nonEmptyString whitespace function emptyObject nonEmptyObject emptyList nonEmptyList greaterZero"
+  );
+
+  /**
+   * 验证函数 本人觉得没有使用场景所以暂时不写了不测了
+   *  */ 
+  // expect(classNames({
+  //   toString: function () { return 'classFromMethod'; }
+  // })).toBe('classFromMethod');
+  // var Class1 = function() {};
+  // var Class2 = function() {};
+  // Class1.prototype.toString = function() { return 'classFromMethod'; }
+  // Class2.prototype = Object.create(Class1.prototype);
+  // expect(classNames(new Class2())).toBe('classFromMethod');
+
+  /**
+  * 测试马老板要的antd类名功能
+  */
+  let prefixCls = 'ant-btn'
+   expect(classNames(prefixCls,{
+    'loading':true,
+    'primary':1,
+    'error':undefined,
+    addPrefixCls:() => {
+      return prefixCls + '-'
+    }
+   },'a')).toBe('ant-btn ant-btn-loading ant-btn-primary a');
+});
